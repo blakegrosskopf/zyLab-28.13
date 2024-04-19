dc8a75bc08df444098f82bd6524880399e4a2862
blakegrosskopf <167143507+blakegrosskopf@users.noreply.github.com>
2024-04-19 13:28:59 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include "ItemToPurchase.h"
 using namespace std;
 
-#include "ItemToPurchase.h"
+// Default constructor
+ItemToPurchase::ItemToPurchase() {
+    itemName = "none";
+    itemPrice = 0;
+    itemQuantity = 0;
+}
 
-/* Type your code here */
+// Mutators
+void ItemToPurchase::SetName(string name) {
+    itemName = name;
+}
+
+void ItemToPurchase::SetPrice(int price) {
+    itemPrice = price;
+}
+
+void ItemToPurchase::SetQuantity(int quantity) {
+    itemQuantity = quantity;
+}
+
+// Accessors
+string ItemToPurchase::GetName() const {
+    return itemName;
+}
+
+int ItemToPurchase::GetPrice() const {
+    return itemPrice;
+}
+
+int ItemToPurchase::GetQuantity() const {
+    return itemQuantity;
+}
